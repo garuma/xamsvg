garuma/xamsvg

commit e68493ff35eb905c320ac23460c862fd077af612

[build] Fix compilation with more recent mcs

Utils/SVGPathParser.cs
@@ -628,6 +628,7 @@ namespace XamSvg.Internals.Utils
 			switch (mCurrentChar) {
 			case '-':
 				mantPos = false;
+				break;
 			case '+':
 				mCurrentChar = this.read ();
 				break;
@@ -660,9 +661,10 @@ namespace XamSvg.Internals.Utils
 
 			mCurrentChar = read ();
 			switch (mCurrentChar) {
-				case '-':
+			case '-':
 				expPos = false;
-				case '+':
+				break;
+			case '+':
 				mCurrentChar = this.read ();
 				break;
 			}

commit 5a7789d207c63ce9d622a3f9e8a753e747cf60e4

Fix infinite loop introduced by e68493ff35e

Utils/SVGPathParser.cs
@@ -628,6 +628,7 @@ namespace XamSvg.Internals.Utils
 			switch (mCurrentChar) {
 			case '-':
 				mantPos = false;
+				mCurrentChar = this.read ();
 				break;
 			case '+':
 				mCurrentChar = this.read ();
@@ -663,6 +664,7 @@ namespace XamSvg.Internals.Utils
 			switch (mCurrentChar) {
 			case '-':
 				expPos = false;
+				mCurrentChar = this.read ();
 				break;
 			case '+':
 				mCurrentChar = this.read ();