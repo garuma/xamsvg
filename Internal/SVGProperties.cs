e645bb0aace9f8b4c50932363db6cb0a044a4aea
Jérémie Laval <jeremie.laval@gmail.com>
2014-02-26 23:25:11 -0500

    [build] Removed unneeded System.Xml usings

--- Internal/SVGProperties.cs
+++ Internal/SVGProperties.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Xml;
 using Attributes = System.Collections.Generic.Dictionary<string, string>;
 
 namespace XamSvg.Internals
--- Utils/SVGParserUtils.cs
+++ Utils/SVGParserUtils.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Xml;
 using Android.Graphics;
 using System.Globalization;
 
